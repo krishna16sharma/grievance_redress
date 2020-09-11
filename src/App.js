c3b48560ff3e1489191af242180fe43ba075fe11
Krishna Sharma <krishna16sharma.s@gmail.com>
2020-09-11 20:42:57 +0530

    Added particles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import Navigation from './components/Navigation/Navigation';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Grievance from './components/grievance_form/grievance_form';
+import grievance1 from "./components/images/Grievance1.png";
+import Particles from 'react-particles-js';
 
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+      line_linked: {
+                width:1,
+                color: "#5C5559"
+            },
+    number: {
+      value: 10,
+      density: {
+        enable: true,
+        value_area: 100
+        }
+    },
+    opacity:0.2,
+    number:{
+        value: 100
+    }
+  }
+}
+
+
+
 const initialState = {
-    current: 'home'
+    current: 'h'
 }
 class App extends React.Component{
     constructor(props){
@@ -22,12 +46,21 @@ class App extends React.Component{
       const {current} = this.state;
       return (
           <div>
+          <Particles className='particles'
+            params={particlesOptions}
+            />
             <Navigation current={{current}} routeChange={this.routeChange}/>
           {
               (current === 'home')
                ?
-                   <div className='home' style={{fontFamily:'Arial'}}>
+                   <div className='home'>
                        <h1>Welcome!</h1>
+                       <div><img src={grievance1} alt="pic"/></div>
+                       <p>Grievance Redressal is a management and governance related process used commonly in India. Grievance Redress Mechanism
+                        is part and parcel of the machinery of any administration.</p>
+                        <p>Our goal is to channel the grievances to the right authorities</p>
+                        <p>We facilitate the resolution of grievances in a fair and impartial manner involving the respective department (dealing
+                         with the substantive function connected with the grievance), maintaining necessary confidentiality, as the case may be.</p>
                    </div>
                 : (current === 'login')
                     ?
